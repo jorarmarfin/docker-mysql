290c7cc0e75f26317f05b6fb41d6f0565083d5e7
Luis mayta <luis.mayta@gmail.com>
2019-07-19 18:20:42 -0500

    moodle

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@
 FROM mysql:5.7
 
 ENV MYSQL_ROOT_PASSWORD root
+ENV MYSQL_DATABASE moodle
+
+ADD moodle.sql  /docker-entrypoint-initdb.d
+
 
 EXPOSE 3306
 
--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-docker build -t lfmc/iso-mysql .
+docker build -t jorarmarfin/mysql:5.7-moodle37 .
+docker build -t jorarmarfin/apache-php:7.2-cron .
+
+docker run --name srv-moodle-mysql -d jorarmarfin/mysql:5.7-moodle37 
+docker run --name srv-moodle-mysql -d -p 8181:3306 jorarmarfin/mysql:5.7-moodle37 
