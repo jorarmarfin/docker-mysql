68fc062f69537fcbc59ae829a86ee6e7ebeced6c
Luis mayta <luis.mayta@gmail.com>
2019-10-24 12:35:43 -0500

    vim watch en mysql

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM mysql:5.7
 
 ENV MYSQL_ROOT_PASSWORD root
 
+RUN apt-get update && apt-get install -y vim watch
+
 EXPOSE 3306
 
 
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t jorarmarfin/mysql:5.7-zabbix .
+docker build -t jorarmarfin/mysql:5.7 .
 
-docker run --name srv-mysql -dit jorarmarfin/mysql:5.7-zabbix
+docker run --name srv-mysql -dit jorarmarfin/mysql:5.7
